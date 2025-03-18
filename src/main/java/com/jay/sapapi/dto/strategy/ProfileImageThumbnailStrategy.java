46be0b9467e00ca6831b15b75c0c8d1495f6f13b
DEOKJAE KWON <juintin@kakao.com>
2025-03-18 15:23:59 +0900

    refactor: modify createThumbnail logic for profile image

--- src/main/java/com/jay/sapapi/dto/strategy/ProfileImageThumbnailStrategy.java
+++ src/main/java/com/jay/sapapi/dto/strategy/ProfileImageThumbnailStrategy.java
@@ -1,7 +1,10 @@
 package com.jay.sapapi.dto.strategy;
 
 import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Path;
 
@@ -9,9 +12,26 @@ public class ProfileImageThumbnailStrategy implements ThumbnailStrategy {
 
     @Override
     public void createThumbnail(Path sourcePath, Path thumbnailPath) throws IOException {
-        Thumbnails.of(sourcePath.toFile())
+        BufferedImage originalImage = ImageIO.read(sourcePath.toFile());
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+
+        int resizedWidth, resizedHeight;
+        if (width < height) {
+            resizedWidth = 200;
+            resizedHeight = (int) ((double) height / width * 200);
+        } else {
+            resizedWidth = (int) ((double) width / height * 200);
+            resizedHeight = 200;
+        }
+
+        BufferedImage resizedImage = Thumbnails.of(originalImage)
+                .size(resizedWidth, resizedHeight)
+                .asBufferedImage();
+
+        Thumbnails.of(resizedImage)
+                .sourceRegion(Positions.CENTER, 200, 200)
                 .size(200, 200)
-                .keepAspectRatio(true)
                 .toFile(thumbnailPath.toFile());
     }
 
