ebc998add81171c14feabdcdca2c746fc4d22047
DEOKJAE KWON <juintin@kakao.com>
2025-03-20 16:44:26 +0900

    feat: add RestDocsConfiguration

---
+++ src/test/java/com/jay/sapapi/config/RestDocsConfiguration.java
@@ -0,0 +1,21 @@
+package com.jay.sapapi.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocumentationResultHandler restDocumentation() {
+        return MockMvcRestDocumentation.document(
+                "{class-name}/{method-name}",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()));
+    }
+
+}
